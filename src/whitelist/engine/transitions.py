4f583f3aa3cfb403f5e0e206f8d6b7bcaae50820
Daham Abeyratne <Dahamabeyratney@gmail.com>
2026-01-25 10:52:04 +0530

    last commit

---
+++ src/whitelist/engine/transitions.py
@@ -0,0 +1,19 @@
+from ..domain.types import Status
+from ..util.timeutils import iso
+
+def apply_transition(record, eval_result, d, settings):
+    hard_pass, beta_ok, score, avg_turn = eval_result
+
+    if hard_pass:
+        record.state.fail_hard_rules_consecutive = 0
+    else:
+        record.state.fail_hard_rules_consecutive += 1
+
+    if record.state.status == Status.TRACK:
+        if hard_pass and beta_ok and score >= settings.whitelist_min_score:
+            record.state.status = Status.WHITELIST
+            record.state.whitelisted_on = iso(d)
+
+    if record.state.fail_hard_rules_consecutive >= settings.churn_fail_hard_n:
+        record.state.status = Status.CHURNED
+        record.state.churned_on = iso(d)
